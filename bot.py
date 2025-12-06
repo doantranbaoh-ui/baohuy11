afd6bc0ff16f0a54cfd3961f83b2cca636f17184
doantranbaoh-ui <doantranbaoh@gmail.com>
2025-12-07 03:20:06 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,7 +3,7 @@ import telebot, sqlite3, threading, time, random, string, secrets, traceback, os
 from keep_alive import keep_alive
 
 # ================= CONFIG =================
-TOKEN = "YOUR_NEW_TOKEN"  # Thay bằng token của bạn
+TOKEN = "6367532329:AAFTX43OlmNc0JpSwOagE8W0P22yOBH0lLU"  # Thay bằng token của bạn
 OWNER_ID = 5736655322
 PRICE_RANDOM = 2000
 DAILY_REPORT_HOUR = 24*60*60  # 24h
