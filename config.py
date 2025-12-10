ccea69f36422f0e99a16f81cd85e627a95cc20cd
doantranbaoh-ui <doantranbaoh@gmail.com>
2025-12-10 08:49:01 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,18 @@
-TOKEN = "6367532329:AAEyb8Uyot8Zj-wBbAyy-ZjJpt4JIeIKGvY"
-ADMIN_ID = 5736655322
+# ============================
+# CONFIG BOT TELEGRAM
+# ============================
 
+# Token bot Telegram của bạn
+TOKEN = "6367532329:AAEyb8Uyot8Zj-wBbAyy-ZjJpt4JIeIKGvY"
+
+# Danh sách Admin (dạng list) → hỗ trợ nhiều admin
+ADMINS = [5736655322]    # ← thay bằng ID của bạn
+
+# Thư mục dữ liệu
 DATA_FOLDER = "data"
+
+# File database
+USER_FILE = f"{DATA_FOLDER}/users.json"
 ACC_FILE = f"{DATA_FOLDER}/acc.txt"
 SOLD_FILE = f"{DATA_FOLDER}/sold_acc.txt"
-USER_FILE = f"{DATA_FOLDER}/users.json"
 BACKUP_FOLDER = f"{DATA_FOLDER}/backup"
