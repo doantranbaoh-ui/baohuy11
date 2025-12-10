94d9590ab8b37cc40cab3087e812baa0abc5ece0
doantranbaoh-ui <doantranbaoh@gmail.com>
2025-12-10 08:39:47 +0700

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -1,11 +1,16 @@
 from fastapi import FastAPI
 import uvicorn
+import threading
 
 app = FastAPI()
 
 @app.get("/")
-def ping():
-    return {"status": "bot is huy"}
+def home():
+    return {"status": "bảo huy dang online nè"}
+
+def run():
+    uvicorn.run(app, host="0.0.0.0", port=10000)
 
 def keep_alive():
-    uvicorn.run(app, host="0.0.0.0", port=10000)
+    thread = threading.Thread(target=run)
+    thread.start()
