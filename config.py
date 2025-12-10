df6c9ec8303f20c488deda56f9600bcbd55a2bdf
doantranbaoh-ui <doantranbaoh@gmail.com>
2025-12-10 10:22:14 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,15 +1,2 @@
-# config.py
 TOKEN = "6367532329:AAFDbKOG4-I8pxo66gF3PPBBzVuxr5xnFUY"
-# List admin ids, ví dụ: [5736655322, 123456789]
-ADMINS = [5736655322]
-
-# Data files
-DATA_FOLDER = "data"
-USERS_FILE = DATA_FOLDER + "/users.json"
-PENDING_FILE = DATA_FOLDER + "/pending.json"
-ACC_FILE = DATA_FOLDER + "/acc.txt"
-SOLD_FILE = DATA_FOLDER + "/sold_acc.txt"
-BACKUP_FOLDER = DATA_FOLDER + "/backup"
-
-# Shop settings
-PRICE_PER_ACC = 2000  # giá 1 acc (đ)
+ADMINS = [5736655322]   # ID Admin
