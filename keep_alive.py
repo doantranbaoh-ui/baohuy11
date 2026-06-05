cacaa2720b0f2cae1f72072ca8466fed78833c06
baohuydz <168200545+baohuydz@users.noreply.github.com>
2026-06-05 23:54:20 +0700

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -1,19 +1,15 @@
-import os
-from threading import Thread
 from flask import Flask
+from threading import Thread
 
-flask_app = Flask('')
+app = Flask('')
 
-@flask_app.route('/')
+@app.route('/')
 def home():
-    return "Hệ thống Shop Liên Quân đang hoạt động ổn định 24/7!", 200
+    return "Bot is running smoothly!"
 
-def run_flask():
-    # Render tự động cấp cổng qua biến môi trường PORT, mặc định là 8080 nếu chạy local
-    port = int(os.environ.get("PORT", 8080))
-    flask_app.run(host='0.0.0.0', port=port)
+def run():
+    app.run(host='0.0.0.0', port=8080)
 
 def keep_alive():
-    t = Thread(target=run_flask)
-    t.daemon = True
+    t = Thread(target=run)
     t.start()
